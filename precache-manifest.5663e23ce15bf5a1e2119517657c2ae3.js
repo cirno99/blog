c03cb44d4c57894cb7c411885c077b8bb2a77755
cirno99 <1563800293@qq.com>
2020-06-15 23:53:54 +0800

    Updates

---
+++ precache-manifest.5663e23ce15bf5a1e2119517657c2ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafa434aa7f5af4455c04f0f1e82e894",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "95dd4a5d6492ba212aae",
+    "url": "/react-blog-github/static/css/main.abb3e5cc.chunk.css"
+  },
+  {
+    "revision": "132a4343daad33aab0be",
+    "url": "/react-blog-github/static/js/2.f4005c1d.chunk.js"
+  },
+  {
+    "revision": "3a1793f6de26a25edda42ca61e6e2a4d",
+    "url": "/react-blog-github/static/js/2.f4005c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95dd4a5d6492ba212aae",
+    "url": "/react-blog-github/static/js/main.b73c179b.chunk.js"
+  },
+  {
+    "revision": "9bf01677584c32cfd019",
+    "url": "/react-blog-github/static/js/runtime-main.7b3fda73.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-blog-github/precache-manifest.630b6cdba4ee23264d3e8e1f9341376c.js"
+  "/react-blog-github/precache-manifest.5663e23ce15bf5a1e2119517657c2ae3.js"
 );
 
 self.addEventListener('message', (event) => {
