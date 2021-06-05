a8621ce36a1797f1b8dbb63e3b4d178092779f20
cirno99 <1563800293@qq.com>
2021-06-05 16:43:28 +0800

    小修改

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,14 +5,12 @@ import {
     HeaderWrapper,
     HeaderTitle,
     HeaderSubtitle,
-    GithubLogin
 } from './'
 import { config } from "../../config"
 
 export const Header = () => {
     return (
         <HeaderContainer>
-            <GithubLogin isAbsolute={true} />
             <HeaderWrapper>
                 <HeaderTitle>{config.title}</HeaderTitle>
                 <HeaderSubtitle>{config.subtitle}</HeaderSubtitle>
--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "Z2hwX0pEdXVvQ0Zqa2pidzlzd01XQXpnM2pmdXhsbXU5ZDFMVGp4NQ==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "cirno99",
 
   // Your Github Repo Name Where You Have your issues as Blog
   githubRepo: "react-blog-github",
@@ -11,21 +11,21 @@ export const config = {
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: 'Iv1.4a59fe8f7c21b2ca',
+  OAuthSecret: '1093a717a103af401f7fa48fe7393810e7992af4',
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "Kiri no Mizuumi" ,
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle : "Underdog",
 
   // Header customization
   header: {
     backgroundColor: '#f1f6f8', // can be a CSS gradient
     backgroundColorDark: '#4C566A',
-    titleColor: '#ff5252',
-    titleColorDark: '#16a085',
+    titleColor: '#30e5ea',
+    titleColorDark: '#f952b7',
     subtitleColor: '#37474f',
     subtitleColorDark: '#D8DEE9',
   },
