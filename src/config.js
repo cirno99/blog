2d2e6599c4d37f67c45aada1362a6c99cb5d35f0
cirno99 <30487226+cirno99@users.noreply.github.com>
2021-11-14 16:05:51 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const config = {
   githubUserName: "cirno99",
 
   // Your Github Repo Name Where You Have your issues as Blog
-  githubRepo: "blog",
+  githubRepo: "react-blog-github",
 
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
